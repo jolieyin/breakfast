19cc4c1b710de7fbfcae9b7ddb606ef2eb3026b2
Jolie Huang <joliehuang@outlook.com>
2020-10-15 11:39:27 +0800

    breakfast_2020.10.15

---
+++ main.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+
+    // Success and Error functions for after the form is submitted
+    
+    function success() {
+      form.reset();
+      status.classList.add('success');
+      status.innerHTML = "美女店長兼主廚肖太太 已收到客官的訂單！";
+    }
+
+    function error() {
+      status.classList.add('error');
+      status.innerHTML = "下單失敗！你沒得吃了！哈哈哈哈哈哈哈";
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function(ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
